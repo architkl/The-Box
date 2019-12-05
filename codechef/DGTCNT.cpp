db525f9ceac55a99c1043eb455fcae4a73a301b5
Archit Kaul <architkl@gmail.com>
2019-12-06 01:14:58 +0530

    Rename

--- codechef/DGTCNT.cpp
+++ codechef/DGTCNT.cpp
@@ -30,18 +30,45 @@ typedef unsigned long long ull;
 typedef long long ll;
 
 vector <int> a(10), low, high, cnt(10);
-int dp[18][10][18][2][2][2];
-int gDGT;
+int temp_dp[18][2][2], dp[1024];
+int gDGT, gMask;
+
 
 void print() {
 	for (int i = 0; i < 10; i++) cout << cnt[i] << " ";
 	cout << "\n";
 }
 
+int dislike(int pos, bool st, bool f1) {
+	if (pos == high.size()) {
+		// print();
+		for (int i = 0; i < 10; i++) if ((gMask & (1 << i)) && cnt[i] != a[i]) return 0;
+		// cout << "FOUND 1\n";
+		return 1;
+	}
+
+	if (temp_dp[pos][st][f1] != -1) return temp_dp[pos][st][f1];
+
+	int res = 0, u_lmt;
+
+	if (f1) u_lmt = high[pos];
+	else u_lmt = 9;
+
+	for (int dgt = 0; dgt <= u_lmt; dgt++) {
+		// cout << dgt << "\n";
+		// cout << pos+1 << " " << dgt << " " << (st || (dgt > 0)) << " " << (f1 || (dgt < u_lmt)) << " " << (f2 || (dgt > l_lmt)) << "\n";
+		cnt[dgt] += (st || (dgt > 0));
+		res += dislike(pos+1, (st || (dgt > 0)), (f1 || (dgt < u_lmt)));
+		cnt[dgt] -= (st || (dgt > 0));;
+	}
+
+	return (temp_dp[pos][st][f1] = res);
+}
+/*
 int like(int pos, int prev, bool st, bool f1, bool f2) {
 	if (pos == high.size()) {
 		// print();
-		for (int i = 0; i < 10; i++) if (cnt[i] == a[i]) return 0;
+		for (int i = 0; i < 10; i++) if (cnt[i] != a[i]) return 0;
 		// cout << "FOUND 1\n";
 		return 1;
 	}
@@ -66,7 +93,7 @@ int like(int pos, int prev, bool st, bool f1, bool f2) {
 
 	return (dp[pos][prev][cnt[prev]][st][f1][f2] = res);
 }
-
+*/
 int solve(ll L, ll R) {
 	low.clear();
 	high.clear();
@@ -83,7 +110,11 @@ int solve(ll L, ll R) {
 
 	memset(dp, -1, sizeof(dp));
 
-	return like(0, 0, 0, 0, 0);
+	ll ans = 0;
+	int temp;
+	for (int i = 1; i < 1024; i++) {
+		gMask = i;
+	}
 }
 
 int main() {
